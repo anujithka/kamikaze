dee06284595c0de057fe95621a357a7b02a3efbb
wrlx <jinutk25846@gmail.com>
2021-05-20 22:01:37 +0530

    UI completed

--- eventmngkamikaze/kamikazeEvents/KAMIKAZEEVENTMNG/urls.py
+++ eventmngkamikaze/kamikazeEvents/KAMIKAZEEVENTMNG/urls.py
@@ -2,4 +2,12 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.hi, name='HOME'),
+    path ('userlogin.html',views.login, name="ulogin"),
+path ('usersignup.html',views.signup, name="usignup"),
+    path('userhome.html',views.home, name="uhome"),
+    path('EventCreator.html',views.ecreate, name="eventcreate"),
+    path('EventRegister.html',views.eregister, name="eventregister"),
+path('EventRegister1.html',views.eregister, name="eventregister1"),
+
+
 ]
--- eventmngkamikaze/kamikazeEvents/KAMIKAZEEVENTMNG/views.py
+++ eventmngkamikaze/kamikazeEvents/KAMIKAZEEVENTMNG/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 def hi(request):
     return render(request,'KAMIKAZEEVENTMNG/First.html')
+def login(request):
+    return  render(request,'KAMIKAZEEVENTMNG/userlogin.html')
+def signup(request):
+    return render(request,'KAMIKAZEEVENTMNG/usersignup.html')
+def home(request):
+    return render(request,'KAMIKAZEEVENTMNG/userhome.html')
+def ecreate(request):
+    return render(request,'KAMIKAZEEVENTMNG/EventCreator.html')
+def eregister(request):
+    return render(request,'KAMIKAZEEVENTMNG/EventRegister.html')
+def eregister(request):
+    return render(request,'KAMIKAZEEVENTMNG/EventRegister1.html')
